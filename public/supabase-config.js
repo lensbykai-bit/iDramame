2eee085c0925d85c435ec02754f7834fb21348fb
lensbykai-bit <lensbykai@gmail.com>
2026-08-31 02:21:51 +0700

    Add iDramaAi Supabase public config

---
+++ public/supabase-config.js
@@ -0,0 +1,5 @@
+window.IDRAMA_SUPABASE = Object.freeze({
+  url: 'https://kbanmyaqodtfoqikzwou.supabase.co',
+  publishableKey: 'sb_publishable_7iqItoUm3UU8Vn24VZvC7Q_0AoTUrzo',
+  checkoutFunction: 'https://kbanmyaqodtfoqikzwou.supabase.co/functions/v1/idrama-checkout'
+});
